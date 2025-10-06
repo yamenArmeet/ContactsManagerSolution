dd1ca344bfd556073e2542909662e771d5dbb287
yamenArmeet <yamen.armeet@gmail.com>
2025-10-06 15:43:23 +0300

    Add project files.

---
+++ ContactsManager.UI/Program.cs
@@ -0,0 +1,81 @@
+﻿using CRUDExample;
+using CRUDExample.Filters.ActionFilters;
+using CRUDExample.Middleware;
+using Entities;
+using Microsoft.EntityFrameworkCore;
+using Repositories;
+using RepositoryContracts;
+using Serilog;
+using ServiceContracts;
+using Services;
+
+var builder = WebApplication.CreateBuilder(args);
+
+//Serilog
+builder.Host.UseSerilog((HostBuilderContext context, IServiceProvider services, LoggerConfiguration loggerConfiguration) => {
+
+    loggerConfiguration
+    .ReadFrom.Configuration(context.Configuration) //read configuration settings from built-in IConfiguration
+    .ReadFrom.Services(services); //read out current app's services and make them available to serilog
+});
+
+
+
+builder.Services.ConfigureServices(builder.Configuration, builder.Environment);
+
+
+var app = builder.Build();
+
+
+
+
+
+//create application pipeline
+if (builder.Environment.IsDevelopment())
+{
+    app.UseDeveloperExceptionPage();
+}
+else
+{
+    app.UseExceptionHandler("/Error");
+    app.UseExceptionHandlingMiddleware();
+}
+
+app.UseHsts();
+app.UseHttpsRedirection();
+
+
+app.UseSerilogRequestLogging();
+
+app.UseHttpLogging();
+
+if (builder.Environment.IsEnvironment("Test") == false)
+    Rotativa.AspNetCore.RotativaConfiguration.Setup("wwwroot", wkhtmltopdfRelativePath: "Rotativa");
+
+app.UseStaticFiles();
+app.UseRouting(); //Identifying action method based route
+
+app.UseAuthentication(); //Reading Identity cookie
+app.UseAuthorization(); //Validates access permissions of the user
+app.MapControllers(); //Execute the filter pipeline(action + filters)
+
+app.UseEndpoints(endpoints => {
+
+    endpoints.MapControllerRoute(
+    name: "areas",
+    pattern: "{area:exists}/{controller=Home}/{action=Index}");
+
+    //Admin/Home/Index
+    //Admin
+
+    endpoints.MapControllerRoute(
+     name: "default",
+     pattern: "{controller}/{action}/{id?}"
+     );
+    //Eg: /persons/edit/1
+});
+
+
+app.Run();
+
+public partial class Program { } //make the auto-generated Program accessible programmatically
